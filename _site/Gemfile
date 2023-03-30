2a277eebef0de36317b4f0131f34c1d5cf5c7deb
khlick <khrisgriffis@gmail.com>
2023-03-29 18:23:05 -0700

    message

--- _site/Gemfile
+++ _site/Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
-gem 'jekyll', '>= 3.1.2'
+gem 'jekyll'
 gem 'wdm', '>= 0.1.0' if Gem.win_platform?
 gem 'github-pages'
+gem 'rouge'
+gem 'webrick'
+gem 'liquid', "4.0.4", path: ".liquid-5.4.0"
--- site.bat
+++ site.bat
@@ -1,8 +1,6 @@
 @echo off
 :: -------------------------------------
 :: Shortcuts to develop, preview, and publish a site
-:: - Using WinSCP to publish from Windows
-::
 :: Example for the _site.env file:
 :: JKL_MYCONFIGS=--config _config.yml,_my/config.yml
 :: JKL_WINSCPPATH=C:\PROGRA~1\WinSCP
@@ -11,23 +9,21 @@
 :: JKL_REMOTEHOST=finesite.example.com
 :: JKL_REMOTEUSER=myremoteuser
 :: JKL_REMOTEEXCLUDES=admin/; anotherapp/; myimportantremotefile.html;
-::
-:: **This last one and the remote site path are IMPORTANT**
-::   This uses 'synchronize', which deletes remote files that are not on
-::   your local site. Be _sure_ to list remote stuff you want to preserve
-::   in JKL_REMOTEEXCLUDES. See https://winscp.net/eng/docs/file_mask
-::   And be sure your remote path is to the web site directory only.
-::   You've been warned. I'm not responsible for loss of data. :-P
 :: -------------------------------------
+IF %2=="" ( SET "message='Update site'" ) ELSE ( SET "message=%2" )
+
 if x%1==x goto :oops
+
+REM parse _site.env
+SET ROOT_DIR = %~dp0
 for /f "tokens=1,2 delims==" %%i in (_site.env) do @set %%i=%%j
 
 if x%1==xdev goto serve
 if x%1==xdevnof goto nofuture
 if x%1==xprod goto prod
-if x%1==xpreview goto preview
 if x%1==xpublish goto write
 echo Nothing to do.
+
 :oops
 echo usage: %~nx0 [dev^|devnof^|prod^|preview^|publish]
 goto :eof
@@ -47,20 +43,17 @@ set JEKYLL_ENV=production
 call bundle exec jekyll build %JKL_MYCONFIGS%
 goto :done
 
-:preview
-set JEKYLL_ENV=production
-call bundle exec jekyll serve %JKL_MYCONFIGS%
-goto :done
-
 :publish
-%JKL_WINSCPPATH%\winscp.com /script=wscp.tmp
-del wscp.tmp
+ECHO "Publishing to "%GH_BRANCH%"."
+call git add .
+call git commit -m message
+call git push origin %BH_BRANCH%
 goto :done
 
 :write
- >wscp.tmp echo open %JKL_REMOTEUSER%^@%JKL_REMOTEHOST%
->>wscp.tmp echo synchronize remote %JKL_LOCALSITEPATH% %JKL_REMOTESITEPATH% -delete -filemask="|%JKL_REMOTEEXCLUDES% site.bat"
->>wscp.tmp echo exit
+set JEKYLL_ENV=production
+call bundle exec jekyll build %JKL_MYCONFIGS% 
+ECHO Building site...
 goto publish
 
 :done
